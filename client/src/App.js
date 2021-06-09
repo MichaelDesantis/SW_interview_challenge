82c72fdc3fdd6929c144aeb06229c5aded4e776f
Michael Desantis <multishifties@yahoo.com>
2021-06-08 23:40:42 -0500

    fix(App): modify file permissions

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+// TODO: Modal for input (file rename)
+// TODO: confirm modal (file delete)
+// TODO: upload page /document/new ?
+// TODO: document listing component
+// TODO: Error component
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [files, setFiles] = React.useState([]);
 
   React.useEffect(() => {
+    // TODO: catch statements, Error handling
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+
+  React.useEffect(() => {
+    fetch("/api/documents")
+      .then((res) => res.json())
+      .then((filesList) => {setFiles(filesList)});
+  }, [files]);
 
 
   return (
@@ -20,6 +33,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{!data ? "Loading..." : data}</p>
+        {(
+          files.length ? 
+            files.map((file) => {
+              return (<p>{file.fileName}</p>)
+            }) : <p>loading...</p>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
--- server/index.js
+++ server/index.js
@@ -8,6 +8,29 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+// TODO: get actual documents
+app.get("/api/documents", (req, res) => {
+  res.json([
+    {fileName: "first.pdf"},
+    {fileName: "second.pdf"},
+    {fileName: "third.pdf"},
+    {fileName: "fourth.pdf"},
+    {fileName: "fifth.pdf"},
+    {fileName: "sixth.pdf"}
+  ]);
+});
+
+// TODO: get individual documents
+// app.get("/api/document/:id", () => {
+
+// });
+
+// API ROUTES NEEDED
+// Upload file (documents and images)
+// Delete file
+// Rename file
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
